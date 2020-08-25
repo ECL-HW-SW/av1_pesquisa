c93b6f6bf18f23f5ab6d42705d90a7600fc02a5d
IcaroGSiqueira <icarogs1998@gmail.com>
2020-08-25 14:01:28 -0300

    fix

--- teste4.sh
+++ teste4.sh
@@ -1,1 +1,1 @@
-aom/build/aomenc -end-usage=q -cq-level=x -lag-in-frames=25 -auto-alt-ref=2 -w 416 -h 240 --limit=2 -o output/bin/teste3.bin ../../videos/av1_sets/bqfree_240p_120f.y4m 2> output/txt/teste3.txt
+aom/build/aomenc --end-usage=q --cq-level=x --lag-in-frames=25 --auto-alt-ref=2 -w 416 -h 240 --limit=2 -o output/bin/teste3.bin ../../videos/av1_sets/bqfree_240p_120f.y4m 2> output/txt/teste3.txt
