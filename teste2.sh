d205a7f0099c36bb8b04fa434e7ae2a3c4f8dcdf
IcaroGSiqueira <icarogs1998@gmail.com>
2020-08-27 11:31:14 -0300

    profiling+fix shell

--- teste2.sh
+++
@@ -1,1 +0,0 @@
-aom/build/aomenc --end-usage=q --cq-level=12 --auto-alt-ref=2 -v --psnr -w 416 -h 240 --limit=2 -o output/bin/teste2.bin ../../videos/av1_sets/bqfree_240p_120f.y4m 2> output/txt/teste2.txt
--- teste4.sh
+++
@@ -1,1 +0,0 @@
-aom/build/aomenc --end-usage=q --cq-level=12 --lag-in-frames=25 --auto-alt-ref=2 -v --psnr -w 416 -h 240 --limit=2 -o output/bin/teste4.bin ../../videos/av1_sets/bqfree_240p_120f.y4m 2> output/txt/teste4.txt
